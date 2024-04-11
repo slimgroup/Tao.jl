1be6ea624c1247f84dadf12cd655ce3b4789e1ab
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-11 10:50:43 -0400

    test

--- examples/3d_fft.jl
+++ examples/3d_fft.jl
@@ -12,20 +12,24 @@ rank = MPI.Comm_rank(comm)
 size = MPI.Comm_size(comm)
 
 CUDA.device!(rank % 4)
-partition = [1, size]
+partition = [1, 1, size]
 
 T = Float32
 
-nt, nx, ny = 10, 10, 10
+# Global Partition
+gt, gx, gy = 100, 100, 100
 
-Ft = ParDFT(T, nt)
-Fx = ParDFT(Complex{T}, nx)
-Fy = ParDFT(Complex{T}, ny)
+# Local Partition, eg: data is sharded across t
+nt, nx, ny = 100 ÷ size, 100, 100
 
-F = Fx ⊗ Ft
+Ft = ParDFT(T, gt)
+Fx = ParDFT(Complex{T}, gx)
+Fy = ParDFT(Complex{T}, gy)
+
+F = Fy ⊗ Fx ⊗ Ft
 F = distribute(F, partition)
 
-x = rand(T, nt, nx) |> gpu
-y = F * x
+x = rand(T, nt, nx, ny) |> gpu
+y = F * vec(x)
 
 MPI.Finalize()
