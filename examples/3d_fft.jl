509475b97de717dd92a3987c7104cde4523e0beb
turquoisedragon2926 <rarockiasamy3@gatech.edu>
2024-04-13 08:29:52 -0400

    update examples

--- examples/3d_fft.jl
+++ examples/3d_fft.jl
@@ -1,33 +1,20 @@
 using Pkg
-Pkg.activate("./")
+Pkg.activate("./path/to/your/environment")
 
 using ParametricOperators
-using CUDA
-using MPI
-
-MPI.Init()
-
-comm = MPI.COMM_WORLD
-rank = MPI.Comm_rank(comm)
-size = MPI.Comm_size(comm)
-
-CUDA.device!(rank % 4)
-partition = [1, 1, size]
 
 T = Float32
 
-# Global and Local Partition
 gt, gx, gy = 100, 100, 100
-nt, nx, ny = [gt, gx, gy] .÷ partition
 
+# Define a transform along each dimension
 Ft = ParDFT(T, gt)
 Fx = ParDFT(Complex{T}, gx)
 Fy = ParDFT(Complex{T}, gy)
 
+# Create a Kronecker operator than chains together the transforms
 F = Fy ⊗ Fx ⊗ Ft
-F = distribute(F, partition)
 
-x = rand(T, nt, nx, ny) |> gpu
+# Apply the transform on a random input
+x = rand(T, gt, gx, gy) |> gpu
 y = F * vec(x)
-
-MPI.Finalize()
